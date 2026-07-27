428b93f390c7dabe909566ee9de19b49e8dba640
myf <mayufei@digit.com.cn>
2026-07-27 19:14:50 +0800

    update

--- apps/web_extension/vite.config.ts
+++ apps/web_extension/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: './', // 【新增】强制 Vite 使用相对路径引用 JS 和 CSS
     build: {
         outDir: 'dist',
         rollupOptions: {
